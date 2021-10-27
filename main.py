9765d1161ae358bda56fc71ee1ccd9d5214b4e5c
catarotaru231 <91543664+catarotaru231@users.noreply.github.com>
2021-10-27 17:46:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def show_menu():
-    print('''
-1. Citire date.
-2. Determinare cea mai lungă subsecvență cu proprietatea ca numerele sunt ordonate crescător.
+    print('''1. Citire date.
+2. Determinare cea mai lungă subsecvență cu proprietatea ca toate numerele sunt palindroame.
 3. Determinare cea mai lungă subsecvență cu proprietatea ca numărul de cifre este în ordine descrescătoare.
 4. Ieșire.
 ''')
@@ -15,6 +14,16 @@ def cifre_ordine_descrescatoare(n):
         x = n % 10
         n //= 10
     return True
+
+def palindrom (n):
+    cn = n
+    s = 0
+    while(n):
+        s = s * 10 + n % 10
+        n//=10
+    if(s == cn):
+        return True
+    return False
 
 def numar_cifre_ord_desc(lst):
     temp = []
@@ -32,37 +41,44 @@ def numar_cifre_ord_desc(lst):
 
 def test_numar_cifre_ord_desc():
     assert numar_cifre_ord_desc([]) == []
-    assert numar_cifre_ord_desc([11,21,31]) == [21,31]
-    assert (numar_cifre_ord_desc([331,321,54231])) == [321]
+    assert numar_cifre_ord_desc([11, 21, 31]) == [21, 31]
+    assert numar_cifre_ord_desc([331, 321, 54231]) == [321]
+    assert numar_cifre_ord_desc([10, 541, 2050, 101]) == [10, 541]
 
-def numere_ordonate_crescator(lst):
-    rez = []
+def get_longest_all_palindromes(lst):
     temp = []
-    for x in range(len(lst)-1):
-        if(lst[x]<lst[x+1]):
-            temp.append(lst[x])
+    rez = []
+    for i in lst:
+        if(palindrom(i)):
+            temp.append(i)
         else:
-            temp.append(lst[x])
-            if(len(temp) > len(rez)):
+            if(len(temp)>len(rez)):
                 rez = temp.copy()
             temp.clear()
-    if (len(temp) > len(rez)):
-        rez = temp.copy()
+        if(len(temp) > len(rez)):
+            rez = temp.copy()
     return rez
-def test_numere_ordonate_crescator():
-    assert numere_ordonate_crescator([]) == []
-    assert numere_ordonate_crescator([1,2,3,1]) == [1,2,3]
+
+
+
+def test_get_longest_all_palindromes():
+    assert get_longest_all_palindromes([]) == []
+    assert get_longest_all_palindromes([11, 22, 33, 121]) == [11, 22, 33, 121]
+    assert get_longest_all_palindromes([10, 102, 204, 506]) == []
+    assert get_longest_all_palindromes([11, 505, 304, 123450, 16]) == [11, 505]
 
 def citire_date():
     n = int(input("Numarul de elemente citite este: "))
     lst = []
     print("Elementele citite sunt: ")
     for i in range(n):
-        ele = int(input())
+        i = str(i)
+        ele = int(input("a["+i+"]= " ))
+        i = int(i)
         lst.append(ele)
     return lst
+test_get_longest_all_palindromes()
 test_numar_cifre_ord_desc()
-test_numere_ordonate_crescator()
 def main():
     lst = []
     while True:
@@ -71,10 +87,10 @@ def main():
         if(P == '1'):
             lst = citire_date()
         elif(P == '2'):
-            print("Secventa maximala de numere ordonate crescator este: ",
-                  numere_ordonate_crescator(lst))
+            print("Secventa maximala de numere palindroame este: ",
+                  get_longest_all_palindromes(lst))
         elif(P == '3'):
-            print("Secventa maximala care are numere cu cifre ordonate crescator este",
+            print("Secventa maximala care are numere cu cifre ordonate descrescator este",
                   numar_cifre_ord_desc(lst))
         elif(P == '4'):
             print("Ati iesit din meniu.")
